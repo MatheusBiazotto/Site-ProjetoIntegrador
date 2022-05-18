461ae4f269b0ec88456d0fc57301a7a4815a33f7
Matheus Biazotto <83847093+MatheusBiazotto@users.noreply.github.com>
2022-05-17 21:08:08 -0300

    Clique duplo na equipe, Loading corrigidos

--- src/views/RacesView.vue
+++ src/views/RacesView.vue
@@ -16,118 +16,83 @@
     <div class="no-data" v-if="data.length === 0 && !loading">
       <h4>Não foram encontrados dados! :c</h4>
       <br />
-      <iframe
-        src="https://embed.lottiefiles.com/animation/99955"
-        frameborder="0"
-      ></iframe>
+      <iframe src="https://embed.lottiefiles.com/animation/99955" frameborder="0"></iframe>
     </div>
-    <div class="main-card card-padding" v-for="ano in years" :key="ano">
-      <div class="card-year">
-        <h2>{{ ano }}</h2>
-        <div class="card-row-hrs">
-          <div class="card-column-hrs">
-            <hr />
-            <hr />
-            <hr />
+    <section id="cards" v-if="data.length !== 0 && !loading">
+      <div class="main-card card-padding" v-for="ano in years" :key="ano">
+        <div class="card-year">
+          <h2>{{ ano }}</h2>
+          <div class="card-row-hrs">
+            <div class="card-column-hrs">
+              <hr />
+              <hr />
+              <hr />
+            </div>
           </div>
         </div>
-      </div>
-      <div class="card-more-info">
-        <div class="card-more-info-teams">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            class="bi bi-people-fill"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
-            />
-            <path
-              fill-rule="evenodd"
-              d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"
-            />
-            <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
-          </svg>
-          <span>{{ filterByYear(data, ano).length }}</span>
-        </div>
+        <div class="card-more-info">
+          <div class="card-more-info-teams">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-people-fill"
+              viewBox="0 0 16 16">
+              <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+              <path fill-rule="evenodd"
+                d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z" />
+              <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
+            </svg>
+            <span>{{ filterByYear(data, ano).length }}</span>
+          </div>
 
-        <span>Clique em uma equipe para visualizar mais informações!</span>
-      </div>
-      <div
-        class="card-padding"
-        v-for="item in filterByYear(data, ano)"
-        :key="item.id"
-      >
-        <!-- Aqui começa a listar as equipes -->
-        <div class="card-list-teams">
-          <img
-            v-if="item.position == 1"
-            v-show="item.position >= 1 && item.position <= 3"
-            src="@/assets/img/first_pos.png"
-            alt="Primeiro lugar"
-          />
-          <img
-            v-if="item.position == 2"
-            v-show="item.position >= 1 && item.position <= 3"
-            src="@/assets/img/sec_pos.png"
-            alt="Segundo lugar"
-          />
-          <img
-            v-if="item.position == 3"
-            v-show="item.position >= 1 && item.position <= 3"
-            src="@/assets/img/thrd_pos.png"
-            alt="Terceiro lugar"
-          />
-          <div class="card-teams-info">
-            <h4
-              v-if="item.position > 3"
-              @click="showMoreInfo(`${item.name}info`)"
-              class="team-title"
-            >
-              {{ `${item.position}° - Equipe ${item.name}` }}
-            </h4>
-            <h4
-              @click="showMoreInfo(`${item.name}info`)"
-              class="team-title"
-              v-else
-            >
-              Equipe - {{ item.name }}
-            </h4>
-            <div title="Média de tempo (menor é melhor)">
-              <ion-icon name="medal-outline" class="ion-medal"></ion-icon>
-              <span>{{ item.mean }}</span>
-              <div class="more-info" :id="`${item.name}info`">
-                <br />
-                <strong>Baterias</strong>
-                <ol>
-                  <li>
-                    <ion-icon name="timer-outline" class="ion-time"></ion-icon>
-                    {{ item.minutes[0] }}:{{ item.seconds[0] }}:{{
-                      item.miliseconds[0]
-                    }}
-                  </li>
-                  <li>
-                    <ion-icon name="timer-outline" class="ion-time"></ion-icon>
-                    {{ item.minutes[1] }}:{{ item.seconds[1] }}:{{
-                      item.miliseconds[1]
-                    }}
-                  </li>
-                  <li>
-                    <ion-icon name="timer-outline" class="ion-time"></ion-icon>
-                    {{ item.minutes[2] }}:{{ item.seconds[2] }}:{{
-                      item.miliseconds[2]
-                    }}
-                  </li>
-                </ol>
+          <span>Clique em uma equipe para visualizar mais informações!</span>
+        </div>
+        <div class="card-padding" v-for="item in filterByYear(data, ano)" :key="item.id">
+          <!-- Aqui começa a listar as equipes -->
+          <div class="card-list-teams">
+            <img v-if="item.position == 1" v-show="item.position >= 1 && item.position <= 3"
+              src="@/assets/img/first_pos.png" alt="Primeiro lugar" />
+            <img v-if="item.position == 2" v-show="item.position >= 1 && item.position <= 3"
+              src="@/assets/img/sec_pos.png" alt="Segundo lugar" />
+            <img v-if="item.position == 3" v-show="item.position >= 1 && item.position <= 3"
+              src="@/assets/img/thrd_pos.png" alt="Terceiro lugar" />
+            <div class="card-teams-info">
+              <h4 v-if="item.position > 3" @click="showMoreInfo(`${item.name}info`)" class="team-title">
+                {{ `${item.position}° - Equipe ${item.name}` }}
+              </h4>
+              <h4 @click="showMoreInfo(`${item.name}info`)" class="team-title" v-else>
+                Equipe - {{ item.name }}
+              </h4>
+              <div title="Média de tempo (menor é melhor)">
+                <ion-icon name="medal-outline" class="ion-medal"></ion-icon>
+                <span>{{ item.mean }}</span>
+                <div class="more-info" :id="`${item.name}info`" style="display:none">
+                  <br />
+                  <strong>Baterias</strong>
+                  <ol>
+                    <li>
+                      <ion-icon name="timer-outline" class="ion-time"></ion-icon>
+                      {{ item.minutes[0] }}:{{ item.seconds[0] }}:{{
+                          item.miliseconds[0]
+                      }}
+                    </li>
+                    <li>
+                      <ion-icon name="timer-outline" class="ion-time"></ion-icon>
+                      {{ item.minutes[1] }}:{{ item.seconds[1] }}:{{
+                          item.miliseconds[1]
+                      }}
+                    </li>
+                    <li>
+                      <ion-icon name="timer-outline" class="ion-time"></ion-icon>
+                      {{ item.minutes[2] }}:{{ item.seconds[2] }}:{{
+                          item.miliseconds[2]
+                      }}
+                    </li>
+                  </ol>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
@@ -261,7 +226,7 @@ body {
   text-align: center;
 }
 
-.next-event > h2 {
+.next-event>h2 {
   color: #ffffff;
   font-weight: 600;
 }
@@ -290,7 +255,7 @@ body {
   align-items: center;
 }
 
-.card-year > h2 {
+.card-year>h2 {
   margin-bottom: 2px;
   font-size: 3rem;
 }
@@ -309,7 +274,7 @@ body {
   justify-content: center;
 }
 
-.card-column-hrs > hr {
+.card-column-hrs>hr {
   width: 55vw;
   margin-bottom: 5px;
   margin-left: 10px;
@@ -324,7 +289,7 @@ body {
   align-items: center;
 }
 
-.card-more-info > span {
+.card-more-info>span {
   background-color: var(--color-mark-green);
   padding: 5px 15px 5px 15px;
   font-size: 1.1rem;
@@ -342,12 +307,12 @@ body {
   align-items: center;
 }
 
-.card-more-info-teams > span {
+.card-more-info-teams>span {
   font-size: 1.5rem;
   font-weight: 300;
 }
 
-.card-more-info-teams > svg {
+.card-more-info-teams>svg {
   color: var(--color-dark-blue);
 }
 
@@ -358,12 +323,12 @@ body {
   gap: 5px;
 }
 
-.card-more-info-date > span {
+.card-more-info-date>span {
   font-size: 1.5rem;
   font-weight: 300;
 }
 
-.card-more-info-date > svg {
+.card-more-info-date>svg {
   color: var(--color-light-green);
 }
 
@@ -381,23 +346,23 @@ body {
   margin-left: 15px;
 }
 
-.card-teams-info > h4 {
+.card-teams-info>h4 {
   font-size: 1.5rem;
   font-weight: bold;
 }
 
-.card-teams-info > div > svg {
+.card-teams-info>div>svg {
   color: var(--color-red);
   font-weight: 100;
   margin-right: 10px;
 }
 
-.card-teams-info > div > span {
+.card-teams-info>div>span {
   font-size: 1rem;
   font-weight: 300;
 }
 
-.card-teams-info > span {
+.card-teams-info>span {
   color: #666666;
   font-size: 0.9rem;
   font-weight: 400;
@@ -411,7 +376,7 @@ body {
   flex-direction: column;
 }
 
-.no-data > iframe {
+.no-data>iframe {
   height: 50%;
 }
 
@@ -449,10 +414,6 @@ ion-icon {
   font-size: 1.5rem;
 }
 
-.more-info {
-  display: none;
-}
-
 .more-info ol {
   padding-left: 14%;
 }
@@ -479,11 +440,11 @@ ion-icon {
     margin-right: 5%;
   }
 
-  .card-column-hrs > hr {
+  .card-column-hrs>hr {
     width: 30vw;
   }
 
-  .card-more-info > span {
+  .card-more-info>span {
     padding: 5px 10px 5px 10px;
     width: auto;
     margin-left: 20px;
